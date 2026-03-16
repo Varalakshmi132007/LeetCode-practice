bf0713848a43afa22e6327e73f99854d0a061e13
Varalakshmi P <varalakshmip630@gmail.com>
2026-03-16 19:44:40 +0530

    Time: 5 ms (19.71%) | Memory: 8.5 MB (65.17%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int temp=x;
+        long long rev = 0;
+        while(x!=0 && x>0)
+        {
+           int r = x%10;
+            rev = (rev*10) + r;
+            x=x/10;
+        }
+       if(rev==temp)
+          return 1;
+       else
+         return 0;
+}
+};
