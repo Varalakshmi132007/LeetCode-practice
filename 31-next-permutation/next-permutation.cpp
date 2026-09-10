9741ac76cdc36b963a6875889c7847508c30039e
Varalakshmi P <varalakshmip630@gmail.com>
2026-09-10 20:44:17 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (82.69%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -3,7 +3,8 @@ public:
     void nextPermutation(vector<int>& A) {
         int n = A.size();
         int pivot = -1;
-
+      
+        //Find the pivot
         for(int i=n-2; i>=0 ;i--){
             if(A[i]<A[i+1]){
                 pivot = i;
@@ -16,13 +17,15 @@ public:
             return;
         }
 
+       //swap the pivot with highest number
        for(int i=n-1; i>pivot ;i--){
         if(A[i]>A[pivot]){
             swap(A[i],A[pivot]);
             break;
         }
        }
-      
+       
+       //reverse the last elements
        reverse(A.begin() + pivot + 1,A.end());
 
     }
